8a9462700ead1215eb49ac242cbbf665b9e28fec
stevenclark92 <steve.clark92@gmail.com>
2017-03-09 19:57:24 +0000

    Added rudimentary macronutrient split with (non-nutritionally researched) support for ketogenic diets

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@ class user(object):
  
     """Quick and dirty calculator to find base metabolic rate & recommended calorie intake based on weight goal"""
  
-    def __init__(self, name, age, weight, height, chromosomes, goal, activity):
+    def __init__(self, name, age, weight, height, chromosomes, goal, activity, diet):
         self.name = name
         self.age = age
         self.weight = weight
@@ -10,6 +10,7 @@ class user(object):
         self.chromosomes = chromosomes
         self.goal = goal
         self.activity = activity
+        self.diet = diet
  
     def base_metabolic_rate(self):
         if self.chromosomes == 'XY':
@@ -44,16 +45,33 @@ class user(object):
             return 0
  
     def body_mass_index(self):
-        return ((self.weight / (self.height ** 2)) * 10000)
+        return (self.weight / (self.height ** 2))
+
+    def macronutrient_split(self):
+        protein = 0
+        carbohydrate = 0
+        fat = 0
+
+        if self.diet == "Keto":
+            protein = (self.calorie_intake() / 2)
+            fat = (self.calorie_intake()/2)
+        else:
+            protein = (self.calorie_intake() * 0.35)
+            carbohydrate = (self.calorie_intake() * 0.35)
+            fat = (self.calorie_intake() * 0.3)
+        return (int(protein), int(carbohydrate), int(fat))
+
 
 # Biology option changed to accept chromosomes at birth - 'XX' for female at birth, 'XY' for male at birth.
 # All units are SI, sorry Americans.
 # Weight goals must be 'Gain', 'Lose', or 'Maintain'
 # Activity Level can be 'Sedentry', 'Light', 'Moderate', 'Active' or 'Extreme'
+# Macros currently supports Keto, any other string returns standard values
 
-user_a = user('test', 22, 79, 172, 'XY', 'Maintain', 'Light')
+user_a = user('test', 22, 79, 172, 'XY', 'Maintain', 'Light', "No Special Requirement")
  
-print(user_a.base_metabolic_rate())
-print(user_a.maintenance_calories())
-print(user_a.calorie_intake())
+print(int(user_a.base_metabolic_rate()))
+print(int(user_a.maintenance_calories()))
+print(int(user_a.calorie_intake()))
 print(user_a.body_mass_index())
+print(user_a.macronutrient_split())
